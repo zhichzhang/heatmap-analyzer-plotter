44753cf5c12256b588af290349af3517b16a070f
zhichzhang <zzhang32@usc.edu>
2025-04-28 16:52:01 -0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import image as mpimg
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import math
+import json
 
 level_width = 380
 level_height = 44
@@ -14,14 +14,36 @@ ceil_size = 4
 num_cols = math.ceil(level_width / ceil_size)
 num_rows = math.ceil(level_height / ceil_size)
 
-level = 0
+# levels = []
+level = ""
 
-EXCEL_PATH = "assets/Team404 Alpha Test Level Time Heatmap  (Responses).xlsx"
+checkpoints = []
+
+EXCEL_PATH = "assets/Team404 Time-cost Heatmap (Responses).xlsx"
+
+
+def get_heatmap_records(data):
+    heatmap_records = []
+
+    for json_str in data['Heatmap JSON String']:
+        data_obj = json.loads(json_str)
+        ceil_data_list = data_obj.get('ceilDataList', [])
+        for entry in ceil_data_list:
+            heatmap_records.append({
+                'Ceil row index': entry['ceilRowIndex'],
+                'Ceil column index': entry['ceilColIndex'],
+                'Time spent on the ceil': entry['time']
+            })
+
+    heatmap_records = pd.DataFrame(heatmap_records)
+
+    return heatmap_records
 
 def one_session_mode():
     session_id = int(input("Select session id: ") or "0")
     global level
 
+    background_img_path = f"assets/levels/{level}_Tilemap.png"
     df = pd.read_excel(EXCEL_PATH)
 
     df.columns = df.columns.str.strip()
@@ -29,17 +51,17 @@ def one_session_mode():
     session_data = df[(df['Session ID'] == session_id) & (df['Level'] == level)]
 
     if session_data.empty:
-        print(f"No data found for the specified session {session_id} and level {level}. Use default values instead.")
-        session_id = 638796696694611015
-        session_data = df[(df['Session ID'] == session_id) & (df['Level'] == level)]
-        print(f"Use session id {session_id} and level {level}.")
+        print(f"No data found for the specified session {session_id} and level {level}.")
+        print("Exit.")
+        return
     else:
         print(f"Data for the specified session {session_id} and level {level} has been found. Generating time-cost heatmap...")
 
 
-    isCompleted = all(session_data['Completed'].unique())
-    session_data = session_data[['Ceil row index', 'Ceil column index', 'Time spent on the ceil']]
-    title = f"Time-cost Heatmap (Session ID = {session_id}, Level = {level}, Level Completed = {isCompleted})"
+    success = any(session_data['Success'].unique())
+
+    session_data = get_heatmap_records(session_data)
+    title = f"Time-cost Heatmap (Session ID = {session_id}, Level = {level}, Success = {success})"
 
     pivot_table = session_data.pivot_table(
         index='Ceil row index',
@@ -51,30 +73,54 @@ def one_session_mode():
 
     pivot_table = pivot_table.reindex(index=range(num_rows), columns=range(num_cols), fill_value=0)
 
-    draw(title, pivot_table)
+    draw(title, pivot_table, background_img_path)
 
 def one_session_one_checkpoint_mode():
     session_id = int(input("Select session id: ") or "0")
-    checkpoint = int(input("Select checkpoint index: ") or "1")
-    global level
 
+    global level
+    background_img_path = f"assets/levels/{level}_Tilemap.png"
+
+    wannaExit = False
     df = pd.read_excel(EXCEL_PATH)
 
     df.columns = df.columns.str.strip()
 
-    checkpoint_data = df[(df['Session ID'] == session_id) & (df['Level'] == level) & (df['Checkpoint'] == checkpoint)]
-    if checkpoint_data.empty:
-        print(f"No data found for the specified session {session_id}, level {level}, and checkpoint {checkpoint}. Use default values instead.")
-        session_id = 638796696694611015
-        checkpoint = 1
-        checkpoint_data = df[(df['Session ID'] == session_id) & (df['Level'] == level) & (df['Checkpoint'] == checkpoint)]
-        print(f"Use the specified session {session_id}, level {level}, and checkpoint {checkpoint}.")
+    session_data = df[(df['Session ID'] == session_id) & (df['Level'] == level)]
+    if session_data.empty:
+        print(f"No data found for the specified session {session_id} and level {level}.")
+        print("Exit.")
+        return
     else:
-        print(f"Data for the specified session {session_id}, level {level}, and checkpoint {checkpoint} has been found. Generating time-cost heatmap...")
+        checkpoints_set = session_data['Checkpoint'].unique()
+        while True:
+            print("Available Checkpoints:")
 
-    isCompleted = all(checkpoint_data['Completed'].unique())
-    checkpoint_data = checkpoint_data[['Ceil row index', 'Ceil column index', 'Time spent on the ceil']]
-    title = f"Time-cost Heatmap (Session ID = {session_id}, Level = {level}, Checkpoint = {checkpoint}, Checkpoint Completed = {isCompleted})"
+            for i, checkpoint in enumerate(checkpoints_set, 1):
+                print(f"{i}. Checkpoint {checkpoint}")
+
+            selected_checkpoint_index = int(input("Select a checkpoint by entering the corresponding number or input -1 to exit: ")) - 1
+
+            if 0 <= selected_checkpoint_index < len(checkpoints_set):
+                checkpoint = checkpoints_set[selected_checkpoint_index]
+                break
+            elif selected_checkpoint_index < 0:
+                wannaExit = True
+                print("Exit.")
+                break
+            else:
+                print(f"No such checkpoint {selected_checkpoint_index}. Please enter a valid checkpoint number.")
+                continue
+        if wannaExit:
+            return
+
+    checkpoint_data = session_data[session_data['Checkpoint'] == checkpoint]
+    print(f"Data for the specified session {session_id}, level {level}, and checkpoint {checkpoint} has been found. Generating time-cost heatmap...")
+
+    success = any(checkpoint_data['Success'].unique())
+    completed = bool(checkpoint_data['Completed'].iloc[0])
+    checkpoint_data = get_heatmap_records(checkpoint_data)
+    title = f"Time-cost Heatmap (Session ID = {session_id}, Level = {level}, Checkpoint = {checkpoint}, Completed = {completed}, Success = {success})"
 
     pivot_table = checkpoint_data.pivot_table(
         index='Ceil row index',
@@ -86,21 +132,22 @@ def one_session_one_checkpoint_mode():
 
     pivot_table = pivot_table.reindex(index=range(num_rows), columns=range(num_cols), fill_value=0)
 
-    draw(title, pivot_table)
+    draw(title, pivot_table, background_img_path)
 
 def all_sessions_mode():
     global level
 
     title = f"Time-cost Heatmap (All Sessions, Level={level})"
+    background_img_path = f"assets/levels/{level}_Tilemap.png"
     df = pd.read_excel(EXCEL_PATH)
+
     df.columns = df.columns.str.strip()
     level_data = df[df['Level'] == level]
     grouped_data = level_data.groupby('Session ID')
     final_data = pd.DataFrame()
 
     for session_id, group in grouped_data:
-        session_data = group[['Ceil row index', 'Ceil column index', 'Time spent on the ceil']]
-
+        session_data = get_heatmap_records(group)
         pivot_table = session_data.pivot_table(
             index='Ceil row index',
             columns='Ceil column index',
@@ -108,33 +155,48 @@ def all_sessions_mode():
             aggfunc='sum',
             fill_value=0
         )
-
         pivot_table = pivot_table.reindex(index=range(num_rows), columns=range(num_cols), fill_value=0)
 
-        scaler = MinMaxScaler()
-        normalized = scaler.fit_transform(pivot_table)
-        normalized_df = pd.DataFrame(normalized, index=pivot_table.index, columns=pivot_table.columns)
-
         if final_data.empty:
-            final_data = normalized_df
+            final_data = pivot_table
         else:
-            final_data += normalized_df
+            final_data += pivot_table
 
-    draw(title, final_data)
+    draw(title, final_data, background_img_path)
 
-def draw(title, table):
-    scalar = MinMaxScaler()
-    normalized = scalar.fit_transform(table)
-    normalized_df = pd.DataFrame(normalized, index=table.index, columns=table.columns)
+
+def draw(title, table, background_img_path):
+    table_log_transformed = np.log1p(table)
+
+    normalized_df = pd.DataFrame(table_log_transformed, index=table.index, columns=table.columns)
 
     fig_width = len(normalized_df.columns) * 0.70
     fig_height = len(normalized_df.index) * 0.85
 
     plt.figure(figsize=(fig_width, fig_height), dpi=100)
 
-    background_img = mpimg.imread('assets/level-0.png')
+    background_img = mpimg.imread(background_img_path)
+    # print(background_img.shape)
 
-    plt.imshow(background_img, aspect='auto', extent=[0, len(normalized_df.columns), 0, len(normalized_df.index)], origin='upper')
+    bg_height, bg_width, _ = background_img.shape
+    required_width = len(normalized_df.columns)
+    required_height = len(normalized_df.index)
+
+    width_diff = math.ceil(bg_width / required_width) * required_width - bg_width
+    height_diff = math.ceil(bg_height / required_height) * required_height - bg_height
+
+    padding_right = width_diff if width_diff > 0 else 0
+    padding_bottom = height_diff if height_diff > 0 else 0
+
+    padded_bg = np.pad(background_img,
+                       ((0, padding_bottom), (0, padding_right), (0, 0)),
+                       mode='constant',
+                       constant_values=(0, 0))
+
+    plt.imshow(padded_bg, interpolation='bilinear', aspect='auto', zorder=0,
+               extent=(0.0, required_width, 0.0, required_height), origin='lower')
+
+    mask = normalized_df == 0
 
     ax = sns.heatmap(
         normalized_df,
@@ -144,33 +206,53 @@ def draw(title, table):
         linecolor='gray',
         cbar=False,
         square=True,
-        alpha=0.6,
+        alpha=0.9,
+        mask=mask,
         xticklabels=True,
         yticklabels=True,
         cbar_kws={"orientation": "horizontal", "pad": 0.1}
     )
 
-    plt.title(title, fontsize=20, fontweight='bold', pad=20, fontname='Arial')
-    ax.set_xlabel("Ceil Column Index", fontsize=16, fontweight='bold', fontname='Arial')
+    plt.title(title, fontsize=32, fontweight='bold', pad=20, fontname='Arial')
+    ax.set_xlabel("Ceil Column Index", fontsize=20, fontweight='bold', fontname='Arial')
     ax.xaxis.set_label_position('bottom')  # 标签移上
     ax.xaxis.tick_top()
-    ax.xaxis.set_label_coords(0.5, -0.06)
-    plt.ylabel("Ceil Row Index", fontsize=16, fontweight='bold', fontname='Arial')
-
+    ax.xaxis.set_label_coords(0.5, -0.03)
+    plt.ylabel("Ceil Row Index", fontsize=20, fontweight='bold', fontname='Arial')
+    plt.tick_params(axis='both', labelsize=16, width=2)
     plt.gca().xaxis.set_ticks_position('bottom')
 
     plt.tight_layout()
 
-    plt.savefig(f"exports/{title}-{ datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.png", dpi=100)
+    plt.savefig(f"exports/{title}-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.png", dpi=100)
     print(f"{title}.png has been generated.")
+
     plt.show()
 
 def init():
     global level
 
-    level = int(input("Select level index (or input -1 to exit): ") or "0")
     df = pd.read_excel(EXCEL_PATH)
+    while True:
+        print("Available levels:")
+
+        levels_set = df['Level'].unique()
+        for i, level_name in enumerate(levels_set, 1):
+            print(f"{i}. Checkpoint {level_name}")
+        level_index = int(input(f"Select a level by entering the corresponding number or or input -1 to exit: ")) - 1
+
+        if 0 <= level_index < len(levels_set):
+            level = levels_set[level_index]
+            break
+        elif level_index < 0:
+            print("Exit.")
+            return False
+        else:
+            print(f"No such level {level_index}. Please enter a valid level number.")
+            continue
+
     level_data = df[df['Level'] == level]
+
     if level_data.empty:
         print(f"No data found for the specified level {level}.")
         return False
@@ -195,7 +277,8 @@ if __name__ == '__main__':
     while True:
         print("-" * 50)
         if not init():
-            print("Initialization failed. Exit.")
+            print("Initialization failed.")
+            print("Exit.")
             break
         choose_mode = int(input("Choose Mode (0: one session, 1: one checkpoint, 2: all session, other: exit): ") or "0")
         if choose_mode == 0:
